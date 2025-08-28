bded53718976f8f88834770d43b5cca07d1c0aa8
Mirrorsan <booranasak.k@cs.rmu.ac.th>
2025-08-28 19:29:03 +0700

    Fix '_exportXlsxByRange' Function

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ===== ของคุณ =====
 window.APP_CONFIG = {
-  API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbx8NAIy0-qX4giKvYxGgnXYjWb_-UajHux2YnT96zAau8NQyLeuPPAzwnimVeZK0rh__g/exec',
-  STATUS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbx8NAIy0-qX4giKvYxGgnXYjWb_-UajHux2YnT96zAau8NQyLeuPPAzwnimVeZK0rh__g/exec?op=formStatus',
+  API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbzStpZWzQuR4BDw87OsEwNO0Mqu2o5fV8vqSsDdBAfOgeuISw3e_eF6EDYh7ueIERD7lg/exec',
+  STATUS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbzStpZWzQuR4BDw87OsEwNO0Mqu2o5fV8vqSsDdBAfOgeuISw3e_eF6EDYh7ueIERD7lg/exec?op=formStatus',
   RECAPTCHA_SITE_KEY: '6LervKorAAAAAGfASzwwujTy77Wa19sNrYepTkdK',
   OAUTH_CLIENT_ID: '696796297498-6nm6iqn4nae01nvsk2eke5e7p9lb53se.apps.googleusercontent.com',
   TIMEZONE: 'Asia/Bangkok',
